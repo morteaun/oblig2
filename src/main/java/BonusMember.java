082ad783a33722b93ed1ef209cabfd8517f812f7
morten <aunemort@gmaiI.com>
2021-02-08 20:38:31 +0100

    Finished BonusMember (Oppgave 2)

--- src/main/java/BonusMember.java
+++ src/main/java/BonusMember.java
@@ -11,37 +11,54 @@ public class BonusMember {
     private static final int SILVER_LIMIT = 25000;
     private static final int GOLD_LIMIT = 75000;
 
-    public int getMemberNumber() {
-        return memberNumber;
+    public BonusMember(int memberNumber, LocalDate enrolledDate, int bonusPoints, String name, String eMailAddress){
+        this.memberNumber = memberNumber;
+        this.enrolledDate = enrolledDate;
+        this.name = name;
+        this.eMailAddress = eMailAddress;
+        this.bonusPointsBalance += bonusPoints;
+        checkAndSetMembership();
     }
 
-    public LocalDate getEnrolledDate() {
-        return enrolledDate;
-    }
+    public int getMemberNumber() { return memberNumber; }
 
-    public int getBonusPointsBalance() {
-        return bonusPointsBalance;
-    }
+    public LocalDate getEnrolledDate() { return enrolledDate; }
 
-    public String getName() {
-        return name;
-    }
+    public int getBonusPointsBalance() { return bonusPointsBalance; }
 
-    public String geteMailAddress() {
-        return eMailAddress;
-    }
+    public String getName() { return name; }
 
-    public String getPassword() {
-        return password;
-    }
+    public String geteMailAddress() { return eMailAddress; }
 
-    public Membership getMembership() {
-        return membership;
-    }
+    public String getPassword() { return password; }
+
+    public Membership getMembership() { return membership; }
+
 
     public boolean checkPassword(String password){
         boolean success = false;
         if (password.equals(getPassword())){success = true;}
         return success;
     }
+
+    public void registerBonusPoints(int newPoints){
+        this.bonusPointsBalance = getMembership().registerPoints(getBonusPointsBalance(),newPoints);
+        checkAndSetMembership();
+    }
+
+    public String getMembershipLevel(){ return getMembership().getMembershipName(); }
+
+    public void checkAndSetMembership(){
+        if (!getMembershipLevel().equals("Gold") && bonusPointsBalance >= GOLD_LIMIT){
+            membership = new GoldMembership();
+        }
+        else if (!getMembershipLevel().equals("Silver") && bonusPointsBalance >= SILVER_LIMIT){
+            membership = new SilverMembership();
+        }
+        else if (!getMembershipLevel().equals("Gold") && !getMembershipLevel().equals("Silver") &&
+                !getMembershipLevel().equals("Basic")){
+            membership = new BasicMembership();
+        }
+    }
+
 }
